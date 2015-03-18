b16b82f04999be498c85f953bbbc86f343ea8dfc
neudabei <rschultzejena@gmail.com>
2015-03-18 09:03:15 +0000

    fixed check_winner method and added readme file

---
+++ readme.md
@@ -0,0 +1,9 @@
+Play Tic, Tac Toe from your terminal:
+
+  | X | O
+-----------
+X | O | X
+-----------
+O | X | O
+
+"You won!"
--- ttt.rb
+++ ttt.rb
@@ -31,11 +31,11 @@ end
 
 def draw_board(b)
   system 'clear'
-  puts " #{b[1]} | #{b[2]} | #{b[3]}" 
-  puts " -----------"
-  puts " #{b[4]} | #{b[5]} | #{b[6]}" 
-  puts " -----------"
-  puts " #{b[7]} | #{b[8]} | #{b[9]}" 
+  puts "#{b[1]} | #{b[2]} | #{b[3]}" 
+  puts "-----------"
+  puts "#{b[4]} | #{b[5]} | #{b[6]}" 
+  puts "-----------"
+  puts "#{b[7]} | #{b[8]} | #{b[9]}" 
 end
 
 def empty_positions(b)
@@ -55,18 +55,27 @@ def computer_picks_square(b)
   b[position] = 'O'
 end
 
+# def check_winner(b)
+#   winning_lines = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[1,4,7],[2,5,8],[3,6,9],[3,5,7]]
+#   winning_lines.each do |line|
+#       if b[line[0]] == 'X' and b[line[1]] == 'X' and b[line[2]] == 'X'
+#         return 'Player'
+#       elsif 
+#         b[line[0]] == 'O' and b[line[1]] == 'O' and b[line[2]] == 'O'
+#         return 'Computer'
+#       else
+#         return nil
+#     end
+#   end
+# end
+
 def check_winner(b)
-  winning_lines = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[1,4,7],[2,5,8],[3,6,9],[3,5,7]]
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
   winning_lines.each do |line|
-      if b[line[0]] == 'X' and b[line[1]] == 'X' and b[line[2]] == 'X'
-        return 'Player'
-      elsif 
-        b[line[0]] == 'O' and b[line[1]] == 'O' and b[line[2]] == 'O'
-        return 'Computer'
-      else
-        return nil
-    end
+    return "Player" if b[line[0]] == 'X' and b[line[1]] == 'X' and b[line[2]] == 'X'
+    return "Computer" if b[line[0]] == 'O' and b[line[1]] == 'O' and b[line[2]] == 'O'
   end
+  nil
 end
 
 board = initialize_board
